bea2fe40e283697a4be8f3fcf76d1cbed138fa8e
Emmanuelle Zenou <emmanuelle.zenou.rogala@gmail.com>
2021-06-24 15:13:15 +0200

    fix bug

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -99,8 +99,8 @@ export default {
     },
     loginWithFB() {
       const provider = new firebase.auth.FacebookAuthProvider();
-      provider.addScope("profile_pic");
-      provider.addScope("name");
+      // provider.addScope("profile_pic");
+      // provider.addScope("name");
 
       firebase
         .auth()
